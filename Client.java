7cc199a5f08e5e3d9da6fa51a9ec1e63eb852db7
Ozan Gokberk <ozan8@hotmail.co.uk>
2023-02-06 18:23:14 +0000

    can have multiple terminals connect to it

--- Client.java
+++ Client.java
@@ -1,37 +1,19 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int ID = 0;
-        System.out.print("Enter the server IP address: ");
-        String serverIP = sc.nextLine();
-        System.out.print("Enter the server port number: ");
-        int port = sc.nextInt();
-        sc.nextLine();
-
-        try (Socket socket = new Socket(serverIP, port)) {
-            System.out.println("Connected to server.");
-            ID = ID + 1;
-            System.out.println("Your ID number is " + ID);
-
-            OutputStream os = socket.getOutputStream();
-            PrintWriter pw = new PrintWriter(os, true);
-            InputStream is = socket.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-            while (true) {
-                System.out.print("Enter a message to send: ");
-                String message = sc.nextLine();
-                pw.println(message);
-                System.out.println("Response from server: " + br.readLine());
-            }
-        } catch (UnknownHostException e) {
-            System.out.println("Error: Could not connect to server.");
-        } catch (IOException e) {
-            System.out.println("Error: Could not send message.");
+        String serverIp = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+        Socket socket = new Socket(serverIp, portNumber);
+        System.out.println("Connected to server " + serverIp + " on port " + portNumber);
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            System.out.print("Enter message to send: ");
+            String message = input.nextLine();
+            // send message to server here
         }
     }
-}   
+}
+    
--- Server.java
+++ Server.java
@@ -1,38 +1,38 @@
-import java.io.*;
-import java.net.*;
-import java.util.*;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
-    public static void main(String[] args) throws IOException {
-        int port = 0;
-        System.out.print("Enter the port number to listen on: ");
-        Scanner sc = new Scanner(System.in);
-        port = sc.nextInt();
-        sc.nextLine();
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.err.println("Usage: java Server <port number>");
+            System.exit(1);
+        }
 
-        try (ServerSocket ss = new ServerSocket(port)) {
-            System.out.println("Server is listening on port " + port);
+        int portNumber = Integer.parseInt(args[0]);
+        try (ServerSocket serverSocket = new ServerSocket(portNumber)) {
+            System.out.println("Server is listening on port " + portNumber);
             while (true) {
-                try (Socket socket = ss.accept()) {
-                    System.out.println("Client connected.");
-                    OutputStream os = socket.getOutputStream();
-                    PrintWriter pw = new PrintWriter(os, true);
-                    InputStream is = socket.getInputStream();
-                    BufferedReader br = new BufferedReader(new InputStreamReader(is));
-
-                    while (true) {
-                        String message = br.readLine();
-                        if (message == null) {
-                            break;
-                        }
-                        pw.println("Message received: " + message);
-                    }
-                } catch (IOException e) {
-                    System.out.println("Error: Could not receive message.");
-                }
+                Socket clientSocket = serverSocket.accept();
+                System.out.println("Accepted connection from client " + clientSocket.getInetAddress().getHostAddress());
+                new ClientHandler(clientSocket).start();
             }
         } catch (IOException e) {
-            System.out.println("Error: Could not listen on port " + port);
+            System.err.println("Could not listen on port " + portNumber);
+            System.exit(1);
         }
     }
-}  
+}
+
+class ClientHandler extends Thread {
+    private Socket clientSocket;
+
+    ClientHandler(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    @Override
+    public void run() {
+        // handle client communication here
+    }
+}
