2fc7b3cb0862a80dc0783fabe9a17dfeb523a5b9
Arda Sahan <ardasahan@Ardas-MacBook-Pro.local>
2023-02-09 13:07:42 +0000

    Added comments

--- Client.java
+++ Client.java
@@ -2,33 +2,46 @@ import java.io.*;
 import java.net.*;
 import java.util.*;
 
+// import required classes for input/output and networking
+
 public class Client {
     public static void main(String[] args) throws IOException {
+        // create a Scanner object to read input from the user
         Scanner sc = new Scanner(System.in);
         int ID = 0;
+        // prompt the user to enter the server IP address
         System.out.print("Enter the server IP address: ");
         String serverIP = sc.nextLine();
+        // prompt the user to enter the server port number
         System.out.print("Enter the server port number: ");
         int port = sc.nextInt();
         sc.nextLine();
 
+        // try to establish a socket connection to the server
         try (Socket socket = new Socket(serverIP, port)) {
             System.out.println("Connected to server.");
             ID = ID + 1;
             System.out.println("Your ID number is " + ID);
 
+            // get the output stream from the socket and create a PrintWriter object
             OutputStream os = socket.getOutputStream();
             PrintWriter pw = new PrintWriter(os, true);
+            // get the input stream from the socket and create a BufferedReader object
             InputStream is = socket.getInputStream();
             BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
+            // infinite loop to continuously prompt the user to enter a message and receive responses from the server
             while (true) {
                 System.out.print("Enter a message to send: ");
                 String message = sc.nextLine();
+                // send the message to the server
                 pw.println(message);
+                // receive and display the response from the server
                 System.out.println("Response from server: " + br.readLine());
             }
-        } catch (UnknownHostException e) {
+        } 
+        // catch exceptions if the server cannot be found or a message cannot be sent
+        catch (UnknownHostException e) {
             System.out.println("Error: Could not connect to server.");
         } catch (IOException e) {
             System.out.println("Error: Could not send message.");
--- Server.java
+++ Server.java
@@ -4,35 +4,60 @@ import java.util.*;
 
 public class Server {
     public static void main(String[] args) throws IOException {
+        // Initialize the port number to 0
         int port = 0;
+
+        // Prompt the user to enter the port number to listen on
         System.out.print("Enter the port number to listen on: ");
+
+        // Read the port number from the user
         Scanner sc = new Scanner(System.in);
         port = sc.nextInt();
+
+        // Consume the newline character after the port number
         sc.nextLine();
 
+        // Try to create a ServerSocket on the specified port
         try (ServerSocket ss = new ServerSocket(port)) {
+            // Print a message indicating that the server is listening on the specified port
             System.out.println("Server is listening on port " + port);
+
+            // Keep listening for incoming connections indefinitely
             while (true) {
+                // Try to accept a new incoming connection
                 try (Socket socket = ss.accept()) {
+                    // Print a message indicating that a client has connected
                     System.out.println("Client connected.");
+
+                    // Get the output stream to send data to the client
                     OutputStream os = socket.getOutputStream();
                     PrintWriter pw = new PrintWriter(os, true);
+
+                    // Get the input stream to receive data from the client
                     InputStream is = socket.getInputStream();
                     BufferedReader br = new BufferedReader(new InputStreamReader(is));
 
+                    // Keep reading messages from the client until the connection is closed
                     while (true) {
+                        // Read the next message from the client
                         String message = br.readLine();
+
+                        // If the message is null, the connection has been closed, so break out of the loop
                         if (message == null) {
                             break;
                         }
+
+                        // Respond to the client by sending back a message indicating that the message was received
                         pw.println("Message received: " + message);
                     }
                 } catch (IOException e) {
+                    // If there was an error receiving the message, print an error message
                     System.out.println("Error: Could not receive message.");
                 }
             }
         } catch (IOException e) {
+            // If there was an error creating the ServerSocket, print an error message
             System.out.println("Error: Could not listen on port " + port);
         }
     }
-}  
+}
