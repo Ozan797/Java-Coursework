1b3094d59a95a773dba1f9a329dd21b864e7dbe9
Arda <ArdaMBP@outlook.com>
2023-02-11 10:50:32 +0000

    fixed chat issues

--- ClientHandler.java
+++ ClientHandler.java
@@ -17,7 +17,7 @@ public class ClientHandler implements Runnable{
             this.bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
             this.clientUsername = bufferedReader.readLine();
             clientHandlers.add(this);
-            broadcastMessage("Server" + clientUsername + " has joined the chat!");
+            broadcastMessage("Server: " + clientUsername + " has joined the chat!");
         } catch (IOException e) {
             closeEverything(socket, bufferedReader, bufferedWriter);
         }
